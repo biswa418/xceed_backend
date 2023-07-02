1648a11782c45024bfd00c055fc08495703dd81d
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-07-02 10:59:21 +0530

    final commit

--- controller/pageController.js
+++ controller/pageController.js
@@ -5,7 +5,7 @@ const jwtAuth = require('../middleware/jwtAuth');
 module.exports.pageWise = async (req, res, next) => {
     const page = Number(req.query.page);
 
-    if (page >= content.length) {
+    if (page === NaN || page >= content.length) {
         return res.status(404).json({
             message: 'Invalid request, data not found',
         })
